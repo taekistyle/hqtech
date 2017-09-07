be0240756ced1611d653a3eeee76a2c3d3aaa4bc
taekistyle <taegi_81@naver.com>
2017-09-07 11:11:38 +0900

    hqtech

---
+++ hqtech/src/main/java/egovframework/com/main/MainController.java
@@ -0,0 +1,62 @@
+package egovframework.com.main;
+
+/**
+ * 컴포넌트 설치 후 설치된 컴포넌트들을 IncludedInfo annotation을 통해 찾아낸 후
+ * 화면에 표시할 정보를 처리하는 Controller 클래스
+ * <Notice>
+ * 		개발시 메뉴 구조가 잡히기 전에 배포파일들에 포함된 공통 컴포넌트들의 목록성 화면에
+ * 		URL을 제공하여 개발자가 편하게 활용하도록 하기 위해 작성된 것으로,
+ * 		실제 운영되는 시스템에서는 적용해서는 안 됨
+ *      실 운영 시에는 삭제해서 배포해도 좋음
+ * <Disclaimer>
+ * 		운영시에 본 컨트롤을 사용하여 메뉴를 구성하는 경우 성능 문제를 일으키거나
+ * 		사용자별 메뉴 구성에 오류를 발생할 수 있음
+ * @author 공통컴포넌트 정진오
+ * @since 2011.08.26
+ * @version 2.0.0
+ * @see
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *  수정일		수정자		수정내용
+ *  -------    	--------    ---------------------------
+ *  2011.08.26	정진오 		최초 생성
+ *  2011.09.16  서준식		컨텐츠 페이지 생성
+ *  2011.09.26  이기하		header, footer 페이지 생성
+ * </pre>
+ */
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController implements ApplicationContextAware, InitializingBean {
+
+	private ApplicationContext applicationContext;
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+
+	public void afterPropertiesSet() throws Exception {}
+
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+		
+		LOGGER.info("EgovComIndexController setApplicationContext method has called!");
+	}
+
+	@RequestMapping("/index.do")
+	public String index(ModelMap model) {
+		return "egovframework/com/main/index";
+	}
+
+	
+}
---
+++ hqtech/src/main/webapp/WEB-INF/jsp/egovframework/com/main/index.jsp
@@ -0,0 +1,2 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+hqtech 하이 퀄리티 테크놀로지
